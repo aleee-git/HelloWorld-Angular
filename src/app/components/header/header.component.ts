c3bebd62a1d1e17ed940170bd666c463ed760f44
aleee-git <alejandraramirez.niu@nehemiahiu.com>
2023-05-12 09:42:20 -0600

    Header Component

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -5,6 +5,28 @@ import { Component } from '@angular/core';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
+
 export class HeaderComponent {
+
+  constructor() {  
+  }
+
+  nombre = "Ale";
+
+  cambiarNombre() {
+    this.nombre = "Cambio 2: Victoria";
+  }
+
+  contador = 0;
+  contar() {
+    // this.contador +=1;
+    this.contador++;
+  }
+
+  array = [1, 2, 3, 4, 5]
+  add() {
+    this.array.push(Math.random());
+  }
+  
 
 }
